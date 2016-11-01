e5fee2cb6c593c38239239fc538a88f50437d525
Josh Broadhurst <broadhurst.josh@gmail.com>
2016-10-31 21:34:09 -0500

    added sidebar right border and integrated react and webpack

--- assets/scripts/sidebar-active.js
+++ assets/scripts/sidebar-active.js
@@ -1,10 +1,8 @@
 $(function() {
     var $page = url('path');
-    console.log($page);
     $('ul.navigation li a').each(function() {
         var $href = $(this).attr('href');
         if ( ($href == $page) || ($href == '') ) {
-            console.log("there");
             $(this).addClass('on');
         } else {
             $(this).removeClass('on');
